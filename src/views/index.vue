9ed9acd6c8b949fe84800c620dc698132dbbe048
GaoYang <gaoyang2@sinorail.com>
2023-07-21 10:26:34 +0800

    - feat: 添加项目信息展示

--- src/views/index.vue
+++ src/views/index.vue
@@ -3,12 +3,28 @@
     <div class="header">
       <div class="title">{{processData.processName}}</div>
       <div class="publish">
-        <el-button type="primary" round @click="publish">发布</el-button>
+        <el-button type="primary" @click="isShowInfo = true" round plain>项目信息</el-button>
+        <el-button type="primary" @click="publish" round>发布</el-button>
       </div>
     </div>
     <div class="body">
       <ProcessDesigner ref="process" :data="processData"/>
     </div>
+
+    <!-- 项目信息 -->
+    <el-dialog v-model="isShowInfo" title="项目信息">
+      <el-descriptions direction="vertical" :column="1" border>
+        <el-descriptions-item label="简介">
+          <p>Easy Process 流程设计器，基于vue3 + vite4 实现，具备低代码、快速应用及扩展的特点。</p>
+          <p>项目完全开源（永久），如果这个项目让你有所收获，记得 Star 关注哦，这对我是非常不错的鼓励与支持。</p>
+          <p>如果在使用过程中发现BUG，请提交Issues。</p>
+        </el-descriptions-item>
+
+        <el-descriptions-item label="开源许可协议">MIT</el-descriptions-item>
+        <el-descriptions-item label="项目地址">https://gitee.com/quxiou-code/easy-process</el-descriptions-item>
+        <el-descriptions-item label="体验入口">http://process.uncodeshop.com</el-descriptions-item>
+      </el-descriptions>
+    </el-dialog>
   </div>
 </template>
 
@@ -22,6 +38,7 @@ import mockData from '~/public/mock/data.json'
 // 审批数据
 let processData = ref({})
 let showDrawer = ref(true)
+let isShowInfo = ref(false)
 
 onMounted(async () => {
   // 获取审批数据
